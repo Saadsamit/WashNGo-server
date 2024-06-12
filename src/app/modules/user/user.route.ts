5895f743a3589682827bf13f8c45817d0a85e613
saadsamit <saadsamit64@gmail.com>
2024-06-13 01:57:08 +0600

    remove extra code

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,7 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { userController } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { loginSchemaValidation, userSchemaValidation } from './user.validation';
-import user from './user.model';
-import sendResponse from '../../utils/sendResponse';
-import auth from '../../middlewares/auth';
-import { User_Role } from './user.const';
 
 const route = Router();
 
@@ -13,21 +9,6 @@ route.post(
   '/signup',
   validateRequest(userSchemaValidation),
   userController.signup,
-);
-
-route.get(
-  '/user',
-  auth(User_Role.user),
-  async (req: Request, res: Response) => {
-    const data = await user.find();
-
-    sendResponse(res, {
-      success: true,
-      statusCode: 200,
-      message: '',
-      data: data,
-    });
-  },
 );
 
 route.post(
